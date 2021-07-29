5e89a5dbe8c042cba8f360b841fe475d0eaf51f2
proioodjcysu <87658353+proioodjcysu@users.noreply.github.com>
2021-07-29 13:47:07 +0700

    Update UnnoBear.bat

--- UnnoBear.bat
+++ UnnoBear.bat
@@ -1,6 +1,6 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"FREE RDP Windows By Gar2007" > out.txt 2>&1
+net config server /srvcomment:"FREE RDP Windows By UnnoBear" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
 net user administrator @Gar2007 /add >nul
@@ -16,6 +16,6 @@ echo Installed , Kalau RDPNya Mati Rebuild Lagi
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak Bisa Menghubungkan Ke NGROK Tunnel,Pastikan NGROK_TOKEN Sudah Benar Di Settings> Secrets> Repository Secret / Mungkin VM Anda Sebelumnya Masih Berjalan: https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
-echo Password: @Gar2007
+echo Password: @UnnoBear
 echo Berhasil Membuat! , Login Ke RDP Anda Sekarang
 ping -n 10 127.0.0.1 >nul
